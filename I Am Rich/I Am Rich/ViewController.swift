490452b973e14dbb9d415c0d9242e147bc0d2588
Siddharth Kulkarni <siddharthkul1995@gmail.com>
2017-12-01 14:20:49 -0800

    I Am Rich
    Basic UILabel and UIImage

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Siddharth Kulkarni on 12/1/17.
+//  Copyright © 2017 Siddharth Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
